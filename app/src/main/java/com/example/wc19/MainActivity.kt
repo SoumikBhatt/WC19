3b46c50ce7b8d9ac24a2c708c3634fd6a404fc50
Soumik Bhattacharjee <soumikcse07@gmail.com>
2019-04-24 00:54:28 +0600

    Initial commit

---
+++ app/src/main/java/com/example/wc19/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.wc19
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.widget.FrameLayout
+import com.example.wc19.fragments.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+
+        nav_bottom.setOnNavigationItemSelectedListener { menuItem ->
+
+            var selectedFragment: Fragment? = null
+
+            when(menuItem.itemId){
+
+                R.id.nav_res_home -> selectedFragment = HomeFragment()
+                R.id.nav_res_teams -> selectedFragment = TeamsFragment()
+                R.id.nav_res_live_tv -> selectedFragment = LiveTVFragment()
+                R.id.nav_res_live_score -> selectedFragment = LiveScoreFragment()
+                R.id.nav_res_live_points_table -> selectedFragment = PointsTableFragment()
+            }
+
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container,selectedFragment!!).commit()
+            true
+
+        }
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/wc19/activities/SplashScreenActivity.kt
@@ -0,0 +1,34 @@
+package com.example.wc19.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
+import com.example.wc19.MainActivity
+import com.example.wc19.R
+
+class SplashScreenActivity:AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 5000 // 5 sec
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_screen_splash)
+
+
+        Handler().postDelayed({
+            // This method will be executed once the timer is over
+            // Start your app main activity
+
+            startActivity(Intent(this, MainActivity::class.java))
+
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
