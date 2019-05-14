9e5d818fd857c79f339bba0738f02c4dd72446b7
Soumik Bhattacharjee <soumikcse07@gmail.com>
2019-05-14 16:17:37 +0600

    MatchDetails page change+ some feature update

--- app/src/main/java/com/example/wc19/utils/AppConst.kt
+++ app/src/main/java/com/example/wc19/utils/AppConst.kt
@@ -3,7 +3,7 @@ package com.example.wc19.utils
 const val LIVE_SCORE_URL = "https://icc-cricket.com/live-cricket/live"
 const val POINTS_TABLE_URL = "https://cricketworldcup.com/standings"
 const val SONY_LIV_URL = "https://sonyliv.com/"
-const val LIVE_STREAM_URL = "https://crichd.com/home7"
+const val LIVE_STREAM_URL = "https://cricket.crichd.com/"
 
 const val DEVELOPER_KEY = "AIzaSyBxB0VumNOiYWTq4iMv8G6K1_LE2qJVn5E"
 const val GTV_LIVE = "9-3rS25gTwk"
--- app/src/main/java/com/example/wc19/view/view/activities/MatchDetailsActivity.kt
+++ app/src/main/java/com/example/wc19/view/view/activities/MatchDetailsActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.View
 import com.example.wc19.R
 import com.example.wc19.model.Match
+import kotlinx.android.synthetic.main.activity_details.*
 import kotlinx.android.synthetic.main.activity_match_details.*
+import kotlinx.android.synthetic.main.activity_match_details.iv_back_details
 
 class MatchDetailsActivity : AppCompatActivity() {
 
@@ -18,34 +20,39 @@ class MatchDetailsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_match_details)
+        setContentView(R.layout.activity_details)
 
         objMatchDetails = intent.getSerializableExtra(DETAILS_KEY) as Match
 
-        tv_team_one_won.text=objMatchDetails.team_one.country
-        tv_team_two_won.text=objMatchDetails.team_two.country
+        tv_match_number.text = objMatchDetails.match_number
+        tv_match_info.text = objMatchDetails.match_time+", "+objMatchDetails.venue
 
-        tv_team_one_win_match.text="Win: "+objMatchDetails.team_one.win
-        tv_team_two_win_match.text="Win: "+objMatchDetails.team_two.win
+        tv_team_name_one.text = objMatchDetails.team_one.country
+        tv_team_name_two.text = objMatchDetails.team_two.country
 
-        tv_team_one_player_name.text=objMatchDetails.team_one.player
-        tv_team_two_player_name.text=objMatchDetails.team_two.player
+        tv_total_match.text = "Matches: "+objMatchDetails.total_match
+        tv_team_one_win.text = objMatchDetails.team_one.country+": "+objMatchDetails.team_one.win
+        tv_team_two_win.text = objMatchDetails.team_two.country+": "+objMatchDetails.team_two.win
+
+        tv_player_one_name.text = objMatchDetails.team_one.player
+        tv_player_two_name.text = objMatchDetails.team_two.player
+
 
         var resources: Resources = applicationContext.resources
 
         val flagOne:Int = resources.getIdentifier(objMatchDetails.team_one.flag,"drawable",applicationContext.packageName)
         val flagTwo:Int = resources.getIdentifier(objMatchDetails.team_two.flag,"drawable",applicationContext.packageName)
 
-        iv_team_one_head.setImageResource(flagOne)
-        iv_team_two_head.setImageResource(flagTwo)
+        iv_flag_team_one.setImageResource(flagOne)
+        iv_flag_team_two.setImageResource(flagTwo)
 
         val playerOne:Int = resources.getIdentifier(objMatchDetails.team_one.player,"drawable",applicationContext.packageName)
         val playerTwo:Int = resources.getIdentifier(objMatchDetails.team_two.player,"drawable",applicationContext.packageName)
 
-        iv_team_one_player.setImageResource(playerOne)
-        iv_team_two_player.setImageResource(playerTwo)
+        iv_player_one.setImageResource(playerOne)
+        iv_player_two.setImageResource(playerTwo)
 
-        iv_back_details.setOnClickListener(View.OnClickListener {
+        iv_back_det.setOnClickListener(View.OnClickListener {
 
             onBackPressed()
         })
